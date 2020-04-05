a50a73b24ae8b7a4a9452eda187824f082b5804d
seishiro.tsukazawa <nantefireda0302@gmail.com>
2020-04-05 06:05:10 +0000

    set login/logout function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:show, :edit]
+  
   def index
   end
 
@@ -7,9 +9,19 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user=User.new
   end
 
   def create
+    @user=User.new(user_params)
+    
+    if @user.save
+      flash[:success]="登録に成功しました"
+      redirect_to @user
+    else
+      flash[:danger]="登録に失敗しました"
+      render :new
+    end
   end
 
   def edit
@@ -19,5 +31,11 @@ class UsersController < ApplicationController
   end
 
   def destroy
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
